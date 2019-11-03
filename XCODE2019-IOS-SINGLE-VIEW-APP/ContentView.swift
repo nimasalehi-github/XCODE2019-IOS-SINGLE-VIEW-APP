249fe14546b449b729ada2d6ce534014036cf5f9
nima salehi <nimasalehi.auth@icloud.com>
2019-11-03 22:00:06 +0330

    Initial Commit

---
+++ XCODE2019-IOS-SINGLE-VIEW-APP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XCODE2019-IOS-SINGLE-VIEW-APP
+//
+//  Created by Nima Salehi on 11/3/19.
+//  Copyright © 2019 Nima Salehi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
