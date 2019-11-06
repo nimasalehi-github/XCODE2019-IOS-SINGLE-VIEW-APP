825a8ee675a5f6a5970617118ef4e93f3d27523e
nima salehi <nimasalehi.auth@icloud.com>
2019-11-06 18:23:49 +0330

    subheadline

--- XCODE2019-IOS-SINGLE-VIEW-APP/ContentView.swift
+++ XCODE2019-IOS-SINGLE-VIEW-APP/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
             VStack {
                 Text("Rooms")
                 Text("People Placeholder")
+                    .font(.subheadline)
             }
         }
     }
