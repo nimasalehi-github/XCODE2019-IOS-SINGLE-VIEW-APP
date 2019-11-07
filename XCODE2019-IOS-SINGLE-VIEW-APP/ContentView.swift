980ba934fc9ad75f2f39210da553691b9548e3bd
nima salehi <nimasalehi.auth@icloud.com>
2019-11-08 00:37:08 +0330

    Okay, so we've got our list and it's working with its cells.  But this view code is a little bit large now.  And I'd really like to factor the cell out to be its own view.  And Xcode helps me do this with one simple operation.  I can just command click on the view that I want and choose Extract Subview.  It's fantastic.  All of my view code gets extracted out down here and I even get to choose the name for my new view.  Let's call it Room Cell.  Thank you.
     Now, let's also add a property for our room.
     And we'll pass it in right here.
     This is a fantastic workflow improvement.
     And with SwiftUI, views are really lightweight so you don't have to worry about creating extra views to better encapsulate or separate your logic.

--- XCODE2019-IOS-SINGLE-VIEW-APP/ContentView.swift
+++ XCODE2019-IOS-SINGLE-VIEW-APP/ContentView.swift
@@ -14,18 +14,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List(rooms) { room in
-                NavigationLink(destination: Text(room.name)) {
-                    
-                Image(room.thumbnailName)
-                    .cornerRadius(15.0)
-                    
-                VStack {
-                    Text(room.name)
-                    Text("\(room.capacity) people")
-                        .font(.subheadline)
-                        .foregroundColor(.secondary)
-                    }
-                }
+                ExtractedViewROOMCELL(room: <#Room#>)
             }
             .navigationBarTitle(/*@START_MENU_TOKEN@*/"Navigation Bar"/*@END_MENU_TOKEN@*/)
         }
@@ -36,5 +25,24 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView(rooms: testData)
         
+    }
+}
+
+struct ExtractedViewROOMCELL: View {
+    let room:Room
+
+    var body: some View {
+        NavigationLink(destination: Text(room.name)) {
+            
+            Image(room.thumbnailName)
+                .cornerRadius(15.0)
+            
+            VStack {
+                Text(room.name)
+                Text("\(room.capacity) people")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+        }
     }
 }
